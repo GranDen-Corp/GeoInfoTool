4ab967f65974c17c078d9e2c657f9a7fed64ab0e
Chen Yu Pao <windperson@gmail.com>
2018-11-29 08:12:39 +0800

    Add Test

--- XUnitTest/CoordinateTransformUnitTest.cs
+++ XUnitTest/CoordinateTransformUnitTest.cs
@@ -7,7 +7,7 @@ namespace XUnitTest
     public class CoordinateTransformUnitTest
     {
         [Fact]
-        public void TestTransformToTuple()
+        public void TestTransformToXyTuple()
         {
             //Arrange
             var referenceOrigin = new ReferenceOriginWgs84Point(new Wgs84Point { Lat = 25.0783615, Lon = 121.5750212 });
@@ -23,6 +23,32 @@ namespace XUnitTest
             var transformTool = new Wgs84Transform(referenceOrigin);
             var transformedOrigin = transformTool.TransformToCartesian(referenceOrigin);
             var transformedTarget = transformTool.TransformToCartesian(targetWgs84Point);
+
+            //Assert
+            Assert.Equal(0.0, transformedOrigin.X, precision);
+            Assert.Equal(0.0, transformedOrigin.Y, 6);
+
+            Assert.Equal(desiredTargetCartesian.X, transformedTarget.X, precision);
+            Assert.Equal(desiredTargetCartesian.Y, transformedTarget.Y, precision);
+        }
+
+        [Fact]
+        public void TestTransformToCartesianPoint()
+        {
+            //Arrange
+            var referenceOrigin = new ReferenceOriginWgs84Point(new Wgs84Point { Lat = 25.0783615, Lon = 121.5750212 });
+            var targetWgs84Point = new Wgs84Point
+            {
+                Lat = 25.0806617,
+                Lon = 121.5749139
+            };
+            var desiredTargetCartesian = new CartesianPoint{X = -10.8250645005394 , Y = 254.80489950534}; 
+            const int precision = 7;
+
+            //Act
+            var transformTool = new Wgs84Transform(referenceOrigin);
+            var transformedOrigin = transformTool.TransformToCartesianPoint(referenceOrigin);
+            var transformedTarget = transformTool.TransformToCartesianPoint(targetWgs84Point);
 
             //Assert
             Assert.Equal(0.0, transformedOrigin.X, precision);
